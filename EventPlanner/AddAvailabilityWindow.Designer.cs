f39a64bddaf9f7bde2b21d6142b36d58ee644684
Liam Ormiston <ljormiston@ku.edu>
2017-10-08 15:19:52 -0700

    fixed gui

--- EventPlanner/AddAvailabilityWindow.Designer.cs
+++ EventPlanner/AddAvailabilityWindow.Designer.cs
@@ -50,10 +50,10 @@
             // 
             // saveAvailabilityButton
             // 
-            this.saveAvailabilityButton.Location = new System.Drawing.Point(42, 834);
-            this.saveAvailabilityButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveAvailabilityButton.Location = new System.Drawing.Point(21, 434);
+            this.saveAvailabilityButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveAvailabilityButton.Name = "saveAvailabilityButton";
-            this.saveAvailabilityButton.Size = new System.Drawing.Size(236, 44);
+            this.saveAvailabilityButton.Size = new System.Drawing.Size(118, 23);
             this.saveAvailabilityButton.TabIndex = 1;
             this.saveAvailabilityButton.Text = "Save";
             this.saveAvailabilityButton.UseVisualStyleBackColor = true;
@@ -63,57 +63,56 @@
             // 
             this.eventComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.eventComboBox.FormattingEnabled = true;
-            this.eventComboBox.Location = new System.Drawing.Point(46, 58);
-            this.eventComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.eventComboBox.Location = new System.Drawing.Point(23, 30);
             this.eventComboBox.Name = "eventComboBox";
-            this.eventComboBox.Size = new System.Drawing.Size(490, 33);
+            this.eventComboBox.Size = new System.Drawing.Size(247, 21);
             this.eventComboBox.TabIndex = 2;
             this.eventComboBox.SelectedIndexChanged += new System.EventHandler(this.eventComboBox_SelectedIndexChanged);
             // 
             // flowPanel
             // 
             this.flowPanel.AutoScroll = true;
-            this.flowPanel.Location = new System.Drawing.Point(42, 254);
-            this.flowPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.flowPanel.Location = new System.Drawing.Point(21, 132);
+            this.flowPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowPanel.Name = "flowPanel";
-            this.flowPanel.Size = new System.Drawing.Size(494, 479);
+            this.flowPanel.Size = new System.Drawing.Size(247, 249);
             this.flowPanel.TabIndex = 3;
             // 
             // briefBox
             // 
-            this.briefBox.Location = new System.Drawing.Point(50, 177);
-            this.briefBox.Margin = new System.Windows.Forms.Padding(4);
+            this.briefBox.Location = new System.Drawing.Point(25, 92);
+            this.briefBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.briefBox.Name = "briefBox";
             this.briefBox.ReadOnly = true;
-            this.briefBox.Size = new System.Drawing.Size(486, 31);
+            this.briefBox.Size = new System.Drawing.Size(245, 20);
             this.briefBox.TabIndex = 4;
             // 
             // locationBox
             // 
-            this.locationBox.Location = new System.Drawing.Point(266, 117);
-            this.locationBox.Margin = new System.Windows.Forms.Padding(4);
+            this.locationBox.Location = new System.Drawing.Point(133, 61);
+            this.locationBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.locationBox.Name = "locationBox";
             this.locationBox.ReadOnly = true;
-            this.locationBox.Size = new System.Drawing.Size(144, 31);
+            this.locationBox.Size = new System.Drawing.Size(74, 20);
             this.locationBox.TabIndex = 5;
             // 
             // hostBox
             // 
-            this.hostBox.Location = new System.Drawing.Point(50, 117);
-            this.hostBox.Margin = new System.Windows.Forms.Padding(4);
+            this.hostBox.Location = new System.Drawing.Point(25, 61);
+            this.hostBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.hostBox.Name = "hostBox";
             this.hostBox.ReadOnly = true;
-            this.hostBox.Size = new System.Drawing.Size(204, 31);
+            this.hostBox.Size = new System.Drawing.Size(104, 20);
             this.hostBox.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 102);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(23, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.Size = new System.Drawing.Size(19, 7);
             this.label1.TabIndex = 7;
             this.label1.Text = "Host";
             // 
@@ -121,10 +120,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(264, 102);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(132, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.Size = new System.Drawing.Size(32, 7);
             this.label2.TabIndex = 8;
             this.label2.Text = "Location";
             // 
@@ -132,32 +131,32 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(48, 158);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(24, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.Size = new System.Drawing.Size(41, 7);
             this.label3.TabIndex = 9;
             this.label3.Text = "Description";
             // 
             // attendeesBox
             // 
-            this.attendeesBox.Location = new System.Drawing.Point(613, 80);
-            this.attendeesBox.Margin = new System.Windows.Forms.Padding(4);
+            this.attendeesBox.Location = new System.Drawing.Point(306, 42);
+            this.attendeesBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.attendeesBox.Multiline = true;
             this.attendeesBox.Name = "attendeesBox";
             this.attendeesBox.ReadOnly = true;
             this.attendeesBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.attendeesBox.Size = new System.Drawing.Size(490, 162);
+            this.attendeesBox.Size = new System.Drawing.Size(247, 86);
             this.attendeesBox.TabIndex = 10;
             // 
             // AttendeesLabel
             // 
             this.AttendeesLabel.AutoSize = true;
             this.AttendeesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AttendeesLabel.Location = new System.Drawing.Point(610, 58);
-            this.AttendeesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AttendeesLabel.Location = new System.Drawing.Point(305, 30);
+            this.AttendeesLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AttendeesLabel.Name = "AttendeesLabel";
-            this.AttendeesLabel.Size = new System.Drawing.Size(72, 17);
+            this.AttendeesLabel.Size = new System.Drawing.Size(37, 7);
             this.AttendeesLabel.TabIndex = 11;
             this.AttendeesLabel.Text = "Attendees";
             // 
@@ -165,20 +164,20 @@
             // 
             this.capacityLabel.AutoSize = true;
             this.capacityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.capacityLabel.Location = new System.Drawing.Point(420, 102);
-            this.capacityLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.capacityLabel.Location = new System.Drawing.Point(210, 53);
+            this.capacityLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.capacityLabel.Name = "capacityLabel";
-            this.capacityLabel.Size = new System.Drawing.Size(62, 17);
+            this.capacityLabel.Size = new System.Drawing.Size(33, 7);
             this.capacityLabel.TabIndex = 13;
             this.capacityLabel.Text = "Capacity";
             // 
             // capacityBox
             // 
-            this.capacityBox.Location = new System.Drawing.Point(422, 117);
-            this.capacityBox.Margin = new System.Windows.Forms.Padding(4);
+            this.capacityBox.Location = new System.Drawing.Point(211, 61);
+            this.capacityBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.capacityBox.Name = "capacityBox";
             this.capacityBox.ReadOnly = true;
-            this.capacityBox.Size = new System.Drawing.Size(114, 31);
+            this.capacityBox.Size = new System.Drawing.Size(59, 20);
             this.capacityBox.TabIndex = 12;
             // 
             // capacityWarning
@@ -186,19 +185,17 @@
             this.capacityWarning.AutoSize = true;
             this.capacityWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.capacityWarning.ForeColor = System.Drawing.Color.DarkGray;
-            this.capacityWarning.Location = new System.Drawing.Point(24, 17);
-            this.capacityWarning.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.capacityWarning.Location = new System.Drawing.Point(12, 9);
             this.capacityWarning.Name = "capacityWarning";
-            this.capacityWarning.Size = new System.Drawing.Size(327, 24);
+            this.capacityWarning.Size = new System.Drawing.Size(184, 13);
             this.capacityWarning.TabIndex = 14;
             this.capacityWarning.Text = "Join Events That Meet Your Schedule";
             // 
             // selectAllButton
             // 
-            this.selectAllButton.Location = new System.Drawing.Point(42, 759);
-            this.selectAllButton.Margin = new System.Windows.Forms.Padding(6);
+            this.selectAllButton.Location = new System.Drawing.Point(21, 395);
             this.selectAllButton.Name = "selectAllButton";
-            this.selectAllButton.Size = new System.Drawing.Size(150, 44);
+            this.selectAllButton.Size = new System.Drawing.Size(75, 23);
             this.selectAllButton.TabIndex = 15;
             this.selectAllButton.Text = "Select All";
             this.selectAllButton.UseVisualStyleBackColor = true;
@@ -206,25 +203,27 @@
             // 
             // taskPanel
             // 
-            this.taskPanel.Location = new System.Drawing.Point(613, 321);
+            this.taskPanel.Location = new System.Drawing.Point(306, 167);
+            this.taskPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.taskPanel.Name = "taskPanel";
-            this.taskPanel.Size = new System.Drawing.Size(490, 412);
+            this.taskPanel.Size = new System.Drawing.Size(245, 214);
             this.taskPanel.TabIndex = 16;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(613, 290);
+            this.label4.Location = new System.Drawing.Point(306, 151);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.Size = new System.Drawing.Size(36, 13);
             this.label4.TabIndex = 17;
             this.label4.Text = "Tasks";
             // 
             // AddAvailabilityWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1240, 1071);
+            this.ClientSize = new System.Drawing.Size(584, 461);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.taskPanel);
             this.Controls.Add(this.selectAllButton);
@@ -243,7 +242,8 @@
             this.Controls.Add(this.eventComboBox);
             this.Controls.Add(this.saveAvailabilityButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximumSize = new System.Drawing.Size(600, 500);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Name = "AddAvailabilityWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Availability";
